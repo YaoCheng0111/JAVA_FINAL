88dea7f81ccc7684f8044bab9e3c9728a7594394
HankWang <xihongw103@gmail.com>
2025-05-29 02:24:32 +0800

    新增一些註解與知會會用到的method

--- HabitTracker/Habit.java
+++ HabitTracker/Habit.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Habit {
     private String name;
     private List<Boolean> checkInStatus;
+    private boolean weeklyAttendence;
 
     public Habit(String name) {
         this.name = name;
@@ -13,16 +14,20 @@ public class Habit {
         for (int i = 0; i < 7; i++) {
             checkInStatus.add(false);
         }
+        this.weeklyAttendence = false;
     }
 
+    //get method of habit name
     public String getName() {
         return name;
     }
 
+    //get method of all days check in
     public List<Boolean> getCheckInStatus() {
         return checkInStatus;
     }
 
+    //*******
     public void setCheckInStatus(List<Boolean> checkInStatus) {
         if (checkInStatus == null || checkInStatus.size() != 7) {
             this.checkInStatus = new ArrayList<>();
@@ -34,11 +39,23 @@ public class Habit {
         }
     }
 
+    //確認當天check in狀態
     public boolean isChecked(int dayIndex) {
         return checkInStatus.get(dayIndex);
     }
 
+    //切換check in 狀態
     public void toggleCheckIn(int dayIndex) {
         checkInStatus.set(dayIndex, !checkInStatus.get(dayIndex));
+    }
+
+    //周全勤
+    public boolean isWeeklyAttendence(){
+        for(int i=0;i<7;i++){
+            if(!isChecked(i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- HabitTracker/HabitManager.java
+++ HabitTracker/HabitManager.java
@@ -6,29 +6,38 @@ import com.google.gson.reflect.TypeToken;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HabitManager {
+    private int completionTotal;
+    private double completionRate;
+    private Boolean[] perfectClockIn = {false,false,false,false,false,false,false};
     private List<Habit> habits = new ArrayList<>();
     private static final String FILE_NAME = "habits.json";
 
+    //從json檔抓檔案
     public HabitManager() {
         loadHabits();
     }
 
+    //add habit to list
     public void addHabit(Habit habit) {
         habits.add(habit);
     }
 
+    //remove habit from list
     public void removeHabit(String habitName) {
         habits.removeIf(h -> h.getName().equals(habitName));
     }
 
+    //get all habit
     public List<Habit> getHabits() {
         return habits;
     }
 
+    //get all habit names
     public List<String> getHabitNames() {
         List<String> names = new ArrayList<>();
         for (Habit h : habits) {
@@ -37,22 +46,67 @@ public class HabitManager {
         return names;
     }
 
+    //依照行獲取habit name
     public String getHabitName(int index) {
         return habits.get(index).getName();
     }
 
+    //get completionTotal
+    public int getCompletionTotal(){
+        completionTotal = 0;
+        for(Habit h:habits){
+            for(int i=0;i<7;i++){
+                if(h.isChecked(i)){
+                    completionTotal++;
+                }
+            }
+        }
+        return completionTotal;
+    }
+
+    //get habit 總數
     public int getHabitCount() {
         return habits.size();
     }
 
-    public boolean isChecked(int row, int col) {
+    //get completionRate
+    public double getCompletionRate(){
+        completionRate = getCompletionTotal() / getHabitCount()*7;
+        return completionRate;
+    }
+
+    //依照table行列找isChecked
+    public Boolean isChecked(int row, int col) {
         return habits.get(row).isChecked(col);
     }
 
+    //當日是否為完美打卡
+    public Boolean isPerfectClockIn(int dayIndex){
+        for(Habit h:habits){
+            if(!h.isChecked(dayIndex)){
+                perfectClockIn[dayIndex]= false;
+                return false;
+            }
+        }
+        perfectClockIn[dayIndex] = true;
+        return true;
+    }
+
+    //完美打卡日數
+    public int getPerfectClockInDays(){
+        int days=0;
+        for(int i=0;i<7;i++){
+            if(isPerfectClockIn(i)) days++;
+        }
+        return days;
+    }
+
+    //依照table行列打勾
     public void toggleCheckIn(int row, int col) {
         habits.get(row).toggleCheckIn(col);
     }
 
+    //存檔
     public void saveHabits() {
         try (FileWriter writer = new FileWriter(FILE_NAME)) {
             new Gson().toJson(habits, writer);
@@ -61,6 +115,7 @@ public class HabitManager {
         }
     }
 
+    //讀檔
     public void loadHabits() {
         try (FileReader reader = new FileReader(FILE_NAME)) {
             Type listType = new TypeToken<List<Habit>>() {
