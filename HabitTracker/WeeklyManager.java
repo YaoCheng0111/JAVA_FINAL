09d471835e67cb019df0f0533d1981e6834fe667
HankWang <xihongw103@gmail.com>
2025-05-29 21:49:55 +0800

    新增WeeklyManager來偵測每次開檔時是否是新的一周

---
+++ HabitTracker/WeeklyManager.java
@@ -0,0 +1,85 @@
+package HabitTracker;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeeklyManager {
+    private Boolean newWeek;
+    private LocalDate today;
+    private LocalDate thisSunday;
+    private List<LocalDate> thisWeek = new ArrayList<>();
+    private static final String FILE_NAME = "week.json";
+
+    //從json檔抓檔案
+    public WeeklyManager() {
+        today = LocalDate.now();
+        thisSunday = today.minusDays(today.getDayOfWeek().getValue() % 7);
+        newWeek = false;
+        loadWeek();
+    }
+
+    //獲取每次打開程式時是否為新的一周
+    public Boolean isNewWeek(){
+        return newWeek;
+    }
+
+    //獲取當周第一天
+    public LocalDate getFirsDate(){
+        return thisWeek.get(0);
+    }
+
+    //獲取當周最後一天
+    public LocalDate getLasDate(){
+        return thisWeek.get(6);
+    }
+
+    //設定成這周七天
+    public void setThisWeek(){
+        thisWeek.clear();
+        for(int i=0;i<7;i++){
+            thisWeek.add(thisSunday.plusDays(i)); 
+        }
+    }
+
+    //存檔
+    public void saveWeek() {
+        try (FileWriter writer = new FileWriter(FILE_NAME)) {
+            new Gson().toJson(thisWeek, writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //讀檔
+    public void loadWeek() {
+        try (FileReader reader = new FileReader(FILE_NAME)) {
+            Type listType = new TypeToken<List<LocalDate>>() {
+            }.getType();
+            List<LocalDate> loaded = new Gson().fromJson(reader, listType);
+            if (loaded != null && !loaded.isEmpty()) {
+                if(loaded.get(0).equals(thisSunday)){//同一周
+                    this.thisWeek = loaded;
+                    this.newWeek = false;
+                    saveWeek();
+                }else{//不同周
+                    this.newWeek = true;
+                    setThisWeek();
+                    saveWeek();
+                }
+            }else{//第一次生成
+                this.newWeek = true;
+                setThisWeek();
+                saveWeek();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
